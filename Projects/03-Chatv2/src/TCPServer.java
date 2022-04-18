6f47fb6e21cfb20c8ff0549d007df6906c09b565
Joao Victor <joaocarvoli@hotmail.com>
2022-04-18 15:34:10 -0300

    removing past configs

--- Projects/03-Chatv2/src/TCPServer.java
+++ Projects/03-Chatv2/src/TCPServer.java
@@ -8,14 +8,12 @@ public class TCPServer {
 			ServerSocket listenSocket = new ServerSocket(serverPort);
 			System.out.println("The server is running on port " + serverPort + "\n");
 			System.out.println("Starting chat: " + "\n");
-			while(true) {
-				Socket clientSocket = listenSocket.accept();
 
-				Input input = new Input(clientSocket, "A");
-				Output output = new Output(clientSocket);
-				input.start();
-				output.start();
-			}
+			Socket clientSocket = listenSocket.accept();
+			Input input = new Input(clientSocket, "A");
+			Output output = new Output(clientSocket);
+			input.start();
+			output.start();
 		} catch(IOException e) {System.out.println("Listen :"+e.getMessage());}
     }
 }
